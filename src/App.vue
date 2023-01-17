55de74958499b7978bf6bebed4066489b4874b61
albertoBarl <alberto.barletta00@gmail.com>
2023-01-17 18:13:31 +0100

    called axios done

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template lang=""></template>
+<script>
+import { store } from "./datas/store.js";
+import axios from "axios";
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  computed: {
+    getMovieList() {
+      axios.get(store.url).then((response) => {
+        store.movieList = response.data.results;
+      });
+    },
+  },
+};
+</script>
+<style lang=""></style>
---
+++ src/datas/store.js
@@ -0,0 +1,5 @@
+import { reactive } from "vue";
+export const store = reactive({
+  url: "https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=a",
+  movieList: [],
+});
