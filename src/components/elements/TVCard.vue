054857dbe83f991d35d02c35a6247fd19bc828e5
albertoBarl <alberto.barletta00@gmail.com>
2023-01-18 12:06:46 +0100

    added and fixed flags

--- src/components/elements/TVCard.vue
+++ src/components/elements/TVCard.vue
@@ -2,8 +2,11 @@
   <div class="myCard bg-danger">
     <h4>{{ tv.name }}</h4>
     <p>"{{ tv.original_name }}"</p>
-    <span>{{ tv.original_language }}</span
-    ><br />
+    <img
+      :alt="`${tv.original_language}`"
+      :src="`https://unpkg.com/language-icons/icons/${tv.original_language}.svg`"
+    />
+    <br />
     <span>{{ tv.vote_average }}</span>
   </div>
 </template>
@@ -18,5 +21,9 @@ export default {
 .myCard {
   width: calc(90% / 5);
   height: 300px;
+  padding: 10px;
+  img {
+    width: 25px;
+  }
 }
 </style>
--- src/components/elements/movieCard.vue
+++ src/components/elements/movieCard.vue
@@ -2,8 +2,11 @@
   <div class="myCard bg-danger">
     <h4>{{ movie.title }}</h4>
     <p>"{{ movie.original_title }}"</p>
-    <span>{{ movie.original_language }}</span
-    ><br />
+    <img
+      :alt="`${movie.original_language}`"
+      :src="`https://unpkg.com/language-icons/icons/${movie.original_language}.svg`"
+    />
+    <br />
     <span>{{ movie.vote_average }}</span>
   </div>
 </template>
@@ -12,11 +15,18 @@ export default {
   props: {
     movie: Object,
   },
+  methods: {},
 };
 </script>
 <style lang="scss" scoped>
 .myCard {
   width: calc(90% / 5);
   height: 300px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  img {
+    width: 25px;
+  }
 }
 </style>
